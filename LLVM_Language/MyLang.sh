9a67720307c59e5c567cecc7e91d1c76c8686a9d
OAMichael <mixa.ovsyann@mail.ru>
2023-12-10 19:11:25 +0300

    Added own language file

--- LLVM_Language/MyLang.sh
+++ LLVM_Language/MyLang.sh
@@ -1,6 +1,6 @@
 bison -d MyLang.y
 lex MyLang.lex
 clang++ lex.yy.c MyLang.tab.c `llvm-config --cppflags --ldflags --libs` -lSDL2 -o MyLang -I/usr/include/SDL2
-cat MyLang.txt
+cat MyLang.lang
 printf "\n"
-cat MyLang.txt | ./MyLang
+cat MyLang.lang | ./MyLang
